0c2e96197659fddf385cbac030763f98a449b244
Johnathan Evanilla <jevanilla@bigelow.org>
2021-06-04 15:15:24 -0400

    Changed es50 calculation at each age to binned years rather than cumulative

--- R/es50_function.R
+++ R/es50_function.R
@@ -14,18 +14,51 @@
 #' 
 calculate_es50 <- function(mpa, key, ages) {
   
-  es50 <- function(age, tbl) {
+  # need to add argument to either calculate cumulatively or binned for each age
+  # using ages 20 and 40 for example, calculate es50 from 0-20, 20-40; or 0-20, 0-40
+  es50 <- function(age, mpa) {
     
-    if (tbl$STATUS_YR + age > 2021) {
-      return("doesn't work")
+    if (mpa$STATUS_YR + age > 2021) {
+      return(NA)
+    } 
+    
+    #filter occurence records for bin calculation
+    if (age == 0) {
+      age_year <- mpa$STATUS_YR + 9
+      
+      age_records <- species_occurence %>% 
+        dplyr::filter(dplyr::between(.data$date_year, mpa$STATUS_YR, age_year))
     } else {
-      return("works")
+      bin_start <- mpa$STATUS_YR + age
+      
+      bin_end <- bin_start + 9
+      
+      age_records <- species_occurence %>% 
+        dplyr::filter(dplyr::between(.data$date_year, bin_start, bin_end))
     }
+    
+    #compute es50 for filtered occurrence records by bin
+    if (nrow(age_records) > 0) {
+      species_counts <- aggregate(age_records$Count, by=list(Category=age_records$scientificName), FUN=sum)
+      
+      species_count_20 <- nrow(species_counts)
+      
+      if (nrow(age_records) >= 50) {
+        es_50 <- vegan::rarefy(species_counts$x, 50)
+        
+        return(es_50)
+      }
+      else {
+        return(NA)
+      }
+    }
+    
   }
   
   hull = st_convex_hull(mpa$geom)
   text = st_as_text(hull)
   
+  #added distinct() to check for duplicates
   species_occurence = try(occurrence(geometry = text))
   
   if (inherits(species_occurence, "try-error")) {
@@ -47,7 +80,7 @@ calculate_es50 <- function(mpa, key, ages) {
     species_count <- aggregate(species_occurence$Count, by=list(Category=species_occurence$scientificName),FUN=sum)
     
     mpa <- mpa %>% 
-      dplyr::mutate(species_count_all = nrow(species_count), .before=geom)
+      dplyr::mutate(species_count_all = nrow(species_count), .before=.data$geom)
     
     if (nrow(species_count) >= 50){
       total_es50 = vegan::rarefy(species_count$x,50)
@@ -56,25 +89,31 @@ calculate_es50 <- function(mpa, key, ages) {
         dplyr::mutate(total_es50 = total_es50, .before=geom)
     }
     
-    es50_by_age <- sapply(ages, es50, mpa)
-    
-    mpa <- mpa %>% 
-      dplyr::mutate(before_10 = es50_by_age[1],
-                    age_0     = es50_by_age[2],
-                    age_10    = es50_by_age[3],
-                    age_20    = es50_by_age[4],
-                    age_40    = es50_by_age[5],
-                    .before=geom)
+    if ("date_year" %in% colnames(species_occurence)) {
+      
+      es50_by_age <- sapply(ages, es50, mpa)
+      
+      mpa <- mpa %>% 
+        dplyr::mutate(age_0     = es50_by_age[1],
+                      age_10    = es50_by_age[2],
+                      age_20    = es50_by_age[3],
+                      age_30    = es50_by_age[4],
+                      age_40    = es50_by_age[5],
+                      age_50    = es50_by_age[6],
+                      age_60    = es50_by_age[7],
+                      .before=geom)
+    }
   }
-  
   return(mpa)
 }
 
 
-#Example script to use calculate_es50() for all mpa records from a country
-country = "Cuba"
 
-ages <- c(-10, 0, 10, 20, 40)
+#Example script to use calculate_es50() for all mpa records from a country
+country = "USA"
+
+#we could dynamically find a list of ages by looking at a summary of the year column after reading the WDPA or in the occurrence records...
+ages <- c(0, 10, 20, 30, 40, 50, 60)
 
 WDPA <- wdpa_read_country(country, team = "ohw-obis-mpa", ext = ".gpkg") %>%
   dplyr::filter(MARINE > 0)
@@ -82,14 +121,15 @@ WDPA <- wdpa_read_country(country, team = "ohw-obis-mpa", ext = ".gpkg") %>%
 x <- WDPA %>% 
   dplyr::select(WDPAID, 
                 NAME, 
-                IUCN_CAT, 
+                IUCN_CAT,
+                REP_AREA,
                 REP_M_AREA, 
                 STATUS, 
                 STATUS_YR, 
                 geom) %>% 
   dplyr::mutate(WKT = st_as_text(geom)) %>% 
   dplyr::group_by(WDPAID) %>% 
-  dplyr::group_map(calculate_es50, ages, .keep=TRUE) %>% 
+  dplyr::group_map(calculate_es50, ages, .keep=TRUE) #%>% 
   dplyr::bind_rows()
 
 
