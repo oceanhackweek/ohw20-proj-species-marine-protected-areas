d757a51ef09cade471f33b174b59245a56ed0544
ngownaris <ngownaris@gmail.com>
2021-08-06 08:37:37 -0400

    Code now pulls + plots times series of MUR SST data

--- server.R
+++ server.R
@@ -115,6 +115,48 @@ server <- function(input, output) {
                 theme_classic()
         })
         
+        
+        output$sst_plot <- renderPlot ({
+            req(input$mpa_select)
+            df <- filter(mpas, WDPAID==input$mpa_select)
+            polygon <- data.frame(sf::st_coordinates(df))
+            
+            Melt_SpatialData <- function(DataOutput, parameter) { #when you use this function, you have to tell R what the dataoutput you want to use is and what the parameter you want to use it
+                latl <- length(DataOutput$latitude)
+                longl <- length(DataOutput$longitude)
+                tl <- length(DataOutput$time)
+                OutputTidy <- melt(DataOutput)
+                OutputTidy$long <- "NA"
+                OutputTidy$lat <- "NA"
+                #there are some extra numbers in the end because of how data are melted -- length of b should be cut off at length of lat * length of long * length of date
+                totall <- latl*longl * tl
+                OutputTidy <- OutputTidy[1:totall,]
+                #have to base these numbers on length of lat, length of long, length of date
+                #long repeats entire vector at length of lat * length of date
+                #lat repeats each number within vector at length of long
+                #date repeats each date within vector at length of lat * length of long
+                OutputTidy$long <- rep(DataOutput$longitude, latl*tl) #long is laid out 1, 2, 3, 1, 2, 3 so don't need each 
+                OutputTidy$lat <- rep(DataOutput$latitude, each = longl) #lat is laid out 1,1,1,,2,2,2 so need each 
+                OutputTidy$time <- rep(DataOutput$time, each = latl * longl)
+                OutputTidy[[parameter]] <- OutputTidy$value
+                return(OutputTidy[,6:9]) #this is getting rid of the unnamed columns for a tidy output
+            }
+            
+            sst_info = rerddap::info('jplMURSST41mday') #note - this is a monthly dataset, then I am averaging across months to get the year average which...isn't great. there are daily datasets but those would take even longer to load. will figure out a solution for this.
+            sst_data <- rerddapXtracto::rxtractogon(sst_info, parameter = "sst", xcoord = polygon$X, ycoord = polygon$Y, tcoord = c("2010-01-01", "2020-12-31")) 
+            sst_tidy <- Melt_SpatialData(DataOutput = sst_data, parameter = "sst") 
+            sst_tidy %>%
+                filter(!is.na(sst)) %>%
+                mutate(year = year(time), sst = as.numeric(sst), year = as.numeric(year)) %>%
+                group_by(year) %>%
+                summarize(mean_SST = mean(sst)) %>% 
+                    ggplot(aes(x = year, y = mean_SST)) + 
+                    geom_point(size = 5, color = "green") + 
+                    scale_x_continuous(breaks = seq(2010, 2020, by = 2)) + 
+                    theme_classic() + 
+                    labs(y = "Average Annual SST", x = "Year")
+        })
+            
         # Render text to appear with results (i.e. the plot and table)
         output$about_this_mpa <- renderUI({
             req(input$mpa_select)
